6cb8cb75ee57e3333a9475fbea94faf48062e11f
Alex <alexander7dk@gmail.com>
2022-02-12 19:45:02 -0800

    Finished 02-variables

---
+++ wisenspace/day1/02-variables.py
@@ -0,0 +1,29 @@
+# string
+
+name = 'Alex'
+print(name)
+print(type(name))
+
+#integer
+
+age = 13 # integer is a whole number
+print(age)
+print(type(age))
+
+#float
+
+weight = 95.61 #float is a decimal point
+print(weight)
+print(type(weight))
+
+#boolean
+
+honest = True
+print(honest)
+print(type(honest))
+
+#None
+
+nothing = None
+print(nothing)
+print(type(nothing))
