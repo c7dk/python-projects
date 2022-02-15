bddc6d31c6d34d773be217616fca6e668992f0e9
Alex <alexander7dk@gmail.com>
2022-02-14 19:16:32 -0800

    Finished fizzbuzz

---
+++ wisenspace/day2/12-fizzbuzz.py
@@ -0,0 +1,11 @@
+# 13. fizzbuzz
+
+for num in range(1,21):
+    if num % 3 == 0 and num % 5 == 0:
+        print('FizzBuzz')
+    elif num % 3 == 0:
+        print('Fizz')
+    elif num % 5 == 0:
+        print('Buzz')
+    else:
+        print(num)
